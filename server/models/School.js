b722f176b31a6b552e61b61cf21a0fafc6a3805e
IGmath2022 <koin7123@gmail.com>
2025-07-10 20:11:23 +0900

    rr

--- server/models/School.js
+++ server/models/School.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
-
 const SchoolSchema = new mongoose.Schema({
   name: { type: String, required: true }
 });
-
-// ★ 학교 일정(기간) virtual 필드 추가!
 SchoolSchema.virtual('SchoolPeriods', {
-  ref: 'SchoolPeriod',                // 참조할 모델
-  localField: '_id',                  // School의 PK
-  foreignField: 'schoolId'            // SchoolPeriod의 FK
+  ref: 'SchoolPeriod',
+  localField: '_id',
+  foreignField: 'schoolId'
 });
 SchoolSchema.set('toObject', { virtuals: true });
 SchoolSchema.set('toJSON', { virtuals: true });
-
 module.exports = mongoose.model('School', SchoolSchema);
