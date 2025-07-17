9179ee2ae48eb8b15f45d4ada75684b7581bac9b
IGmath2022 <koin7123@gmail.com>
2025-07-17 19:29:56 +0900

    qi

--- server/utils/alimtalk.js
+++ server/utils/alimtalk.js
@@ -1,15 +1,12 @@
 const axios = require('axios');
 
-// 알리고(카카오) 템플릿 상수
-const TITLE_TEMPLATE = `#{name}학생이 #{type}하였습니다`;
-
+const TITLE_TEMPLATE = '#{name}학생이 #{type}하였습니다';
 const BODY_TEMPLATE = `IG수학입니다.
 #{name} 학생이 #{type} 하였습니다. (#{time})
 #{automsg}
 
 - 본 메시지는 자동발송입니다.`;
 
-// 변수 치환 함수 (알리고의 템플릿 변수명을 그대로 사용!)
 function fillTemplate(template, variables = {}) {
   return template
     .replace('#{name}', variables.name ?? '')
@@ -19,7 +16,7 @@ function fillTemplate(template, variables = {}) {
 }
 
 exports.sendAlimtalk = async (phone, template_code, variables = {}) => {
-  const subject = fillTemplate(TITLE_TEMPLATE, variables);
+  const emtitle = fillTemplate(TITLE_TEMPLATE, variables);   // 강조표기형 타이틀
   const message = fillTemplate(BODY_TEMPLATE, variables);
 
   const params = {
@@ -30,9 +27,9 @@ exports.sendAlimtalk = async (phone, template_code, variables = {}) => {
     sender: process.env.ALIGO_SENDER,
     receiver_1: phone,
     recvname_1: variables.name ?? '',
-    subject_1: subject,
+    emtitle_1: emtitle,        // ← subject_1이 아니라 emtitle_1로 반드시 입력!
     message_1: message,
-    // button_1, failover 등 필요시 추가 가능
+    // button_1 등 필요시 추가
   };
 
   try {
