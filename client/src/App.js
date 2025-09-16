f6d394d0706d00c04728b86d886edcb29edf1f55
IGmath2022 <koin7123@gmail.com>
2025-09-17 00:47:56 +0900

    Reapply "2025.09.17(3)"
    This reverts commit 9272ec49b5b2dd172aaefb4197fcc2f1196ab07c.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
-// client/src/App.js
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -14,13 +12,11 @@ import Blog from "./pages/Blog";
 import AttendancePage from "./pages/AttendancePage";
 import ReportPublic from "./pages/ReportPublic";
 
-// ⚠️ 문제가 생기면 이 import만 실패시키고 초기 렌더는 살립니다.
-const SuperSiteSettings = lazy(() => import("./pages/SuperSiteSettings"));
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* NavBar/FloatingContact 있는 Layout */}
         <Route element={<Layout />}>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
@@ -30,20 +26,14 @@ function App() {
           <Route path="/materials" element={<Materials />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/blog" element={<Blog />} />
-          <Route
-            path="/super-settings"
-            element={
-              <Suspense fallback={<div style={{padding:20}}>로딩 중…</div>}>
-                <SuperSiteSettings />
-              </Suspense>
-            }
-          />
         </Route>
 
+        {/* NavBar/FloatingContact 없는 Layout */}
         <Route path="/attendancePage" element={<Layout hideNavBar={true} />}>
           <Route index element={<AttendancePage />} />
         </Route>
 
+        {/* 공개 리포트 뷰 */}
         <Route path="/r/:code" element={<Layout hideNavBar={true} />}>
           <Route index element={<ReportPublic />} />
         </Route>
